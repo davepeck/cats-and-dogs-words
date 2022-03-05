6b6e85db21aea3fcc8e44c5b798ac85d0e59baaf
Dave <davepeck@gmail.com>
2022-03-05 12:52:32 -0800

    Age old.

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,17 @@ export const App: React.FC = () => {
   return (
     <div>
       <h1>Cat &amp; Dog Word</h1>
+      <h6>
+        By{" "}
+        <a href="https://cats-and-dogs.club/" target="_blank">
+          Ellie
+        </a>{" "}
+        (age 7) &amp;{" "}
+        <a href="https://davepeck.org/" target="_blank">
+          daddy
+        </a>{" "}
+        (age old).
+      </h6>
       <Keyboard
         letterClues={{}}
         onLetterKey={() => {}}
