7e2512d927a40b8c5cfd8d616eb57c3d9757a592
Dave <davepeck@gmail.com>
2022-03-06 18:15:37 -0800

    Better keyboard layout on phone

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { Keyboard } from "./keyboard";
 export const App: React.FC = () => {
   return (
     <div>
-      <h1>Cat &amp; Dog Word</h1>
+      <h1 className="title">Cat &amp; Dog Word</h1>
       <h6 className="subtitle">
         By{" "}
         <a href="https://cats-and-dogs.club/" target="_blank">
@@ -34,7 +34,7 @@ export const App: React.FC = () => {
         <Row length={5} state="future" word="" clues={[]} />
         <Row length={5} state="future" word="" clues={[]} />
       </Grid>
-      <div style={{ marginTop: "2em" }}>
+      <div style={{ marginTop: "3em" }}>
         <Keyboard
           letterClues={{ a: "incorrect", b: "correct", c: "move" }}
           onLetterKey={() => {}}
